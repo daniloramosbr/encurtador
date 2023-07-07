5dd3c5ad4d1f0eca52b306babdc787c2955f4dc1
Danilo Ramos <125937280+daniloramosbr@users.noreply.github.com>
2023-07-06 21:56:01 -0300

    atualiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import RoutesApp from "./routes.jsx";
+import Home from "./pages/home";
 
 function App() {
   return (
     <div>
-     oi
+      <Home/>
     </div>
   );
 }
